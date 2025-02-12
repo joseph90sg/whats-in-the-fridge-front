d2d00f64c2e67cbfb7fbd52d71cee9d721c0ae15
Joseph <joseph90sg@gmail.com>
2025-02-12 01:08:57 +0100

    feat(form): add components to build a form

--- src/components/atoms/inputs/InputField.tsx
+++ src/components/atoms/inputs/InputField.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { TextInputProps } from "@/components/types/inputTypes";
 
 interface InputFieldProps extends TextInputProps {
@@ -15,23 +14,3 @@ export default function InputField({ id, value, onChange, type = "text", isRequi
             onChange={onChange} />
   );
 }
-||||||| parent of 7401202 (feat(inputs): add inputField component)
-=======
-interface InputFieldProps {
-  id: string;
-  label: string;
-  type?: string;
-  isRequired?: boolean;
-  minLength?: number;
-  maxLength?: number;
-}
-
-export default function InputField({ id, label, type = "text", isRequired = false, minLength = 0, maxLength }: InputFieldProps) {
-  return (
-    <>
-      <label htmlFor={id}>{label} { isRequired && <span className="text-red-500">*</span> }</label>
-      <input id={id} type={type} minLength={minLength} maxLength={maxLength} required={isRequired}></input>
-    </>
-  );
-}
->>>>>>> 7401202 (feat(inputs): add inputField component)
